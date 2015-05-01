725b2234bad9ecad2e344d73054aa9c8e29b1e9c
Andrew Horner <andrew@tablexi.com>
2015-05-01 16:23:41 -0500

    Determine lookups on either side of an association
    To provide better support for polymorphic mappings (generally in the
    form of STI), we need to allow for `belongs_to` relationships to define
    a single lookup which corresponds to a generic `has_many` association
    on the target record.

--- lib/restforce/db/associations/has_one.rb
+++ lib/restforce/db/associations/has_one.rb
@@ -21,7 +21,7 @@ module Restforce
           @cache = cache
 
           target = target_mapping(database_record)
-          query = "#{lookup} = '#{salesforce_record.Id}'"
+          query = "#{lookup_field(target, database_record)} = '#{salesforce_record.Id}'"
 
           instance = target.salesforce_record_type.first(query)
           instance ? construct_for(database_record, instance) : []
