1f69522c86993dfebdab7aca6e5f295d543517bd
Andrew Horner <andrew@tablexi.com>
2015-05-20 17:30:16 -0500

    Synchronize deletion of records from Salesforce
    When a record is deleted from Salesforce, it is maintained for about 15
    days in an archive in their system, which we can query on each run. As
    we identify new records which have been deleted, we should drop them 
    from our system, mirroring the actual state of the data.

--- lib/restforce/db/cleaner.rb
+++ lib/restforce/db/cleaner.rb
@@ -3,8 +3,14 @@ module Restforce
   module DB
 
     # Restforce::DB::Cleaner is responsible for culling the matching database
-    # records when a Salesforce record no longer meets the sync conditions.
+    # records when a Salesforce record has been deleted or otherwise no longer
+    # meets the sync conditions for a mapping.
     class Cleaner
+
+      # Salesforce can take a few minutes to register record deletion. This
+      # buffer gives us a window of time (in seconds) to look back and see
+      # records which may not have been visible in previous runs.
+      DELETION_READ_BUFFER = 3 * 60
 
       # Public: Initialize a Restforce::DB::Cleaner.
       #
@@ -20,11 +26,44 @@ module Restforce
       #
       # Returns nothing.
       def run
+        drop_deleted_records
+        drop_invalid_records
+      end
+
+      private
+
+      # Internal: Destroy any database records corresponding to Salesforce
+      # records which have been deleted within the Salesforce environment.
+      #
+      # Returns nothing.
+      def drop_deleted_records
+        return unless @runner.after
+        @mapping.database_record_type.destroy_all(deleted_salesforce_ids)
+      end
+
+      # Internal: Destroy any database records corresponding to Salesforce
+      # records which have been modified such that they no longer meet the
+      # conditions defined for this mapping.
+      #
+      # Returns nothing.
+      def drop_invalid_records
         return if @mapping.conditions.empty? || @strategy.passive?
         @mapping.database_record_type.destroy_all(invalid_salesforce_ids)
       end
 
-      private
+      # Internal: Get the IDs of records which have been removed from Salesforce
+      # for this mapping within the DELETION_BUFFER for this run.
+      #
+      # Returns an Array of IDs.
+      def deleted_salesforce_ids
+        response = Restforce::DB.client.get_deleted_between(
+          @mapping.salesforce_model,
+          @runner.after - DELETION_READ_BUFFER,
+          @runner.before,
+        )
+
+        response.deletedRecords.map(&:id)
+      end
 
       # Internal: Get the IDs of records which are in the larger collection
       # of Salesforce records, but which do not meet the specific conditions for
